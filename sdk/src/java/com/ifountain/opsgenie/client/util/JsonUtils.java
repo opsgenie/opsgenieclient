e08f7d1ad2e04d319a97a253b61234612c8b7b6e
mustafa <mustafa.sener@ifountain.com>
2013-10-09 12:19:22 +0300

    amazon upgrade

--- sdk/src/java/com/ifountain/opsgenie/client/util/JsonUtils.java
+++ sdk/src/java/com/ifountain/opsgenie/client/util/JsonUtils.java
@@ -1,6 +1,6 @@
 package com.ifountain.opsgenie.client.util;
 
-import org.codehaus.jackson.map.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.Map;
