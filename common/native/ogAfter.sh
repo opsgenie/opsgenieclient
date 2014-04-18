5bfb6453ea0fa936ca3f69b1ae22761c109b0d89
mustafa <mustafa.sener@ifountain.com>
2014-04-18 15:12:59 +0300

    client native build fix

---
+++ common/native/ogAfter.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+chmod -R 755 /var/log/opsgenie
+chmod -R g+s /var/log/opsgenie
+chmod -R 755 /var/lib/opsgenie
+chmod -R 755 /etc/opsgenie
+chmod -R 755 /var/opsgenie
+
+chown -R opsgenie:opsgenie /etc/opsgenie
+chown -R opsgenie:opsgenie /var/log/opsgenie
+chown -R opsgenie:opsgenie /var/lib/opsgenie
+chown -R opsgenie:opsgenie /var/opsgenie
--- marid/ogBefore.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [  -z $(getent passwd opsgenie) ]
-then
-  useradd opsgenie
-fi
