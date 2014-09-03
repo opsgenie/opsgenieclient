155fd78b68c8e66cf2c170f193cafb3b380d72e1
tubaozturk <tubaozturk92@gmail.com>
2014-09-03 15:15:15 +0300

    rpm fix

--- common/native/ogAfterUnInstall.sh
+++ common/native/ogAfterUnInstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf /etc/opsgenie/lamp/*
-rm -rf /var/lib/opsgenie/lamp/*
-rm -rf /etc/opsgenie/marid/*
-rm -rf /var/lib/opsgenie/marid/*
+#rm -rf /etc/opsgenie/lamp/*
+#rm -rf /var/lib/opsgenie/lamp/*
+#rm -rf /etc/opsgenie/marid/*
+#rm -rf /var/lib/opsgenie/marid/*
