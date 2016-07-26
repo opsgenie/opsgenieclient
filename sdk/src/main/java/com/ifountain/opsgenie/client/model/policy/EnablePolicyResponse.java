718542e3d00d2a1e2f7622e71ad764e4d3ba9f47
mehmetdemircs <mehmetdemircs@gmail.com>
2016-07-26 14:22:37 +0300

    policy api is added

---
+++ sdk/src/main/java/com/ifountain/opsgenie/client/model/policy/EnablePolicyResponse.java
@@ -0,0 +1,12 @@
+package com.ifountain.opsgenie.client.model.policy;
+
+import com.ifountain.opsgenie.client.model.BaseResponse;
+
+
+/**
+ * Represents OpsGenie service response for enable/disable policy request.
+ *
+ * @see com.ifountain.opsgenie.client.IPolicyOpsGenieClient#enablePolicy(EnablePolicyRequest)
+ */
+public class EnablePolicyResponse extends BaseResponse{
+}
