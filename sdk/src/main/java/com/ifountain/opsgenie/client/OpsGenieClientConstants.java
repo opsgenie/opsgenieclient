c297bfb058ef5792affaa97351c65ec9ba488aec
mehmetdemircs <mehmetdemircs@gmail.com>
2016-07-29 16:30:51 +0300

    day of week constants fixed

--- sdk/src/main/java/com/ifountain/opsgenie/client/OpsGenieClientConstants.java
+++ sdk/src/main/java/com/ifountain/opsgenie/client/OpsGenieClientConstants.java
@@ -182,13 +182,13 @@ public interface OpsGenieClientConstants {
         public static final String SMS = "sms";
 
         //Day of Week Enum
-        public static final String SUNDAY ="sunday";
-        public static final String MONDAY = "monday";
-        public static final String TUESDAY = "tuesday";
-        public static final String WEDNESDAY = "wednesday";
-        public static final String THURSDAY = "thursday";
-        public static final String FRIDAY = "friday";
-        public static final String SATURDAY = "saturday";
+        public static final String SUNDAY ="SUNDAY";
+        public static final String MONDAY = "MONDAY";
+        public static final String TUESDAY = "TUESDAY";
+        public static final String WEDNESDAY = "WEDNESDAY";
+        public static final String THURSDAY = "THURSDAY";
+        public static final String FRIDAY = "FRIDAY";
+        public static final String SATURDAY = "SATURDAY";
         
     }
 }
