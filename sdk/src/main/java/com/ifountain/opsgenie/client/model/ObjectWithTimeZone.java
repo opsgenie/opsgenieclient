9418547d852d14f98a26bb8f9fe7c04a8a67d8cb
Mehmet Mustafa Demir <mehmetdemircs@gmail.com>
2016-11-22 04:31:22 +0300

    Add snoozerequest timezone property

--- sdk/src/main/java/com/ifountain/opsgenie/client/model/ObjectWithTimeZone.java
+++ sdk/src/main/java/com/ifountain/opsgenie/client/model/ObjectWithTimeZone.java
@@ -4,6 +4,15 @@ import org.codehaus.jackson.annotate.JsonIgnore;
 
 import java.util.TimeZone;
 
+/**
+ * This interface made for models(beans) which have time(date) and time zone objects.
+ * Current client SDK parsing those objects with given timezone.
+ * To do it sdk firstly parse the JSON to model.
+ * After that it parse id again with the timezone of the object.
+ * In this way sdk parse the model according to the time zone of the model.
+ *
+ * @see com.ifountain.opsgenie.client.util.JsonUtils
+ */
 public interface ObjectWithTimeZone {
     @JsonIgnore
     TimeZone getObjectTimeZone();
